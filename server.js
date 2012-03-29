bffe2bb66cad5a407a10f8f8106495ebcb7dcd16
Jerry <sjerrys@gmail.com>
2012-03-29 17:04:18 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var daemon = require('daemon');
+
+require("debug");
+
+
+daemon.daemonize('tmp/stdout-and-stderr.log', 'tmp/shop.pid', function (err, pid) {
+    //
+    // We are now in the daemon process
+    //
+    if (err) {
+      return console.log('Error starting daemon: ' + err);
+    }
+
+    console.log('Daemon started successfully with pid: ' + pid);
+  });
